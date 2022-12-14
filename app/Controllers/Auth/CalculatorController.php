527277236c4cd0f7f086019567f790c63c26bab5
Jorge Olate <jorge.olate1@cloud.uautonoma.cl>
2022-12-14 02:52:37 -0300

    cambios en calculator

---
+++ app/Controllers/Auth/CalculatorController.php
@@ -0,0 +1,36 @@
+<?php
+namespace App\Controllers\Auth;
+
+use CodeIgniter\Controller;
+use Config\Email;
+use Config\Services;
+use App\Models\VentaModel;
+use App\Models\LogsModel;
+
+class CalculatorController extends Controller
+{
+    /**
+	 * Access to current session.
+	 *
+	 * @var \CodeIgniter\Session\Session
+	 */
+	protected $session;
+
+	/**
+	 * Authentication settings.
+	 */
+	protected $config;
+
+
+    //--------------------------------------------------------------------
+
+	public function __construct()
+	{
+		// start session
+		$this->session = Services::session();
+	}
+
+    //--------------------------------------------------------------------
+
+    
+}
